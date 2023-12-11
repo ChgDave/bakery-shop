e38e0bd812c0e27621fb0253b759a086cc2ac038
ChgDave <chgdave@gmail.com>
2023-12-10 18:59:56 -0600

    my code

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -68,7 +68,7 @@ export default function Cart() {
             console.log("Chcekc out!");
           }}
         >
-          Chcekout
+          Checkout
         </Button>
       </div>
     </div>
